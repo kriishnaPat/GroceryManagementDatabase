729f2bc1551aa1c1b3bad45c57faeb448e1f5218
Krishna Patel <kriishnapat@gmail.com>
2022-05-04 10:08:47 -0600

    ewrwer

---
+++ Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace Data_Managment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            String[] products = System.IO.File.ReadAllLines(@"data-files/grocery.txt");
+            Console.WriteLine("***DICTIONARY***"); printStringArray(products, 0, 50);
+            while (true){
+                //diplays menu options
+                Console.WriteLine(@"
+1.Display all of the products
+2.Display certain products
+3.Select products to add to shopping cart.
+4.Remove products from shopping cart.
+5.Display shopping cart.
+6.Logout
+");
+            Console.Write("Enter the number for your choice: ");
+            int user_choice = Convert.ToInt32(Console.ReadLine());
+            if (user_choice == 1){
+                
+            } 
+            else if (user_choice == 2){
+            }
+            else if (user_choice == 3){ 
+            }
+            else if (user_choice == 4){
+            }
+            else if (user_choice == 5){
+            }
+            else if (user_choice == 6){
+                break;
+            }
+            else {
+                Console.Write("That was not a valid choice!");
+            }
+        }
+
+
+
+
+
+    }
+  }
+}
+
