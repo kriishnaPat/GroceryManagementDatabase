ae00157e48c5b43f7143f0b0df10c6885a8f018d
krishna-pat <63011441+krishna-pat@users.noreply.github.com>
2022-05-20 23:38:11 -0600

    woot woot no eerororor

--- Program.cs
+++ Program.cs
@@ -7,14 +7,46 @@ using System.Collections.Generic;
 
 namespace Data_Managment
 {
-
+static class MainList{
+            static List<List<List<string>>> userChoice;
+            
+            static MainList()
+            {
+                // Allocate the list.
+                userChoice = new List<List<List<string>>>();
+            }
+            
+            public static void Record(List<List<string>> value)
+            {
+                // Record this value in the list.
+                userChoice.Add(value);
+            }
+            public static void Remove(List<List<string>> value){
+                userChoice.Remove(value);
+            }
     class Program
-    {
-        public static void Main(string[] args)
+    {       
+        public  static void Main(string[] args)
         {
             //load stuff on file already
             String groceryText = File.ReadAllText(@"data-files/grocery.txt");
             String[] items = Regex.Split(groceryText, @"/");
+            
+            static void retrieveFile(){
+                string jsonStringFromFile = File.ReadAllText(@"data-files/data.txt");
+                List<List<List<string>>> data = JsonSerializer.Deserialize<List<List<List<string>>>>(jsonStringFromFile);
+                Console.Write(jsonStringFromFile);
+                if (jsonStringFromFile == " ")
+                {
+                    List<List<List<string>>> userChoice = new List<List<List<string>>>();
+                    usercheck(userChoice);
+                }
+                else
+                {
+                    List<List<List<string>>> userChoice = JsonSerializer.Deserialize<List<List<List<string>>>>(jsonStringFromFile);
+                    usercheck(userChoice);
+                }
+            }
             retrieveFile();
             while (true)
             {
@@ -55,32 +87,28 @@ namespace Data_Managment
                         {
                             Console.WriteLine(items[i]);
                         }
-                        else
-                        {
-                            Console.WriteLine("Sorry that was an invalid category number please try again.");
-                        }
                     }
                 }
                 else if (user_choice == 3)
                 {
-                    retrieveFile();
                     List<string> shoppingCart = new List<string>();
                     Console.Write(@"Please enter the name or product number, for the product you would like to add to your shopping cart: ");
                     string want = Console.ReadLine();
-                    for (int i = 0; i < 5; i++)
+                    for (int i = 0; i < items.Length; i++)
                     {
                         if (items[i].Contains(want) == true)
                         {
+                            Console.WriteLine(items[i]);
                             Console.Write($"{items[i]}Is this what you are looking for, press 1 for yes or 2 for no:");
                             string yn = Console.ReadLine();
                             if (yn == "1")
                             {
-                                string want = items[i];
-                                shoppingCart.Add(want);
-                                userChoice[i].Add(shoppingCart);
+                                string itemWanted = items[i];
+                                shoppingCart.Add(itemWanted);
                             }
                         }
                     }
+                    userChoice[0].Add(shoppingCart);
                 }
                 else if (user_choice == 4)
                 {
@@ -162,22 +190,7 @@ namespace Data_Managment
                     }
             }
         }
-            static void retrieveFile(){
-                string jsonStringFromFile = File.ReadAllText(@"data-files/data.txt");
-                List<List<List<string>>> data = JsonSerializer.Deserialize<List<List<List<string>>>>(jsonStringFromFile);
-                Console.Write(jsonStringFromFile);
-                if (jsonStringFromFile == " ")
-                {
-                    public static List<List<List<string>>> userChoice = new List<List<List<string>>>();
-                    usercheck(userChoice);
-
-                }
-                else
-                {
-                    public static List<List<List<string>>> userChoice = JsonSerializer.Deserialize<List<List<List<string>>>>(jsonStringFromFile);
-                    usercheck(userChoice);
-                }
-            }
+            
         //     static void editCart(List<string> items)
         //     {
         //         List<string> shoppingCart = new List<string>();
@@ -203,5 +216,4 @@ namespace Data_Managment
     }
 }
 }
-
-
+}
